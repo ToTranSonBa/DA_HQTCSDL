9438fe50e14d1d36e2d6f380af57cc16662a949d
Nguyễn Quốc Anh <92172767+QuocMaChe@users.noreply.github.com>
2022-10-30 21:31:23 +0700

    Update proc.sql
    update tài xế

--- DATABASE/proc.sql
+++ DATABASE/proc.sql
@@ -35,6 +35,27 @@ grant select
 on TINHTRANGDONHANG
 to KhachHang
 
+--Tạo role và phân quyền cho tài xế
+create role TaiXe
+go
+
+Grant Insert, Update
+on TAIXE
+to TaiXe
+go
+
+Grant Select, Insert, Update
+on TINHTRANGDONHANG
+to TaiXe
+go
+
+Grant Select
+on DONHANG
+to TaiXe
+go
+
+--Chức năng của các role
+--Khách hàng
 --thêm thông tin cho khách hàng mới
 create proc sp_ThemThongTinKhachHang
 	@tenkh nvarchar(10),
@@ -456,3 +477,264 @@ begin tran
 		rollback tran
 	end catch
 commit tran
+--Tài xế
+--Chức năng của tài xế
+--Thêm thông tin tài xế
+create proc sp_themthongtintaixe
+@ten nvarchar(10),
+@cmnd char(20),
+@sdt char(10),
+@bien_so_xe char(10),
+@stk char(20),
+@ngan_hang nvarchar(100),
+@gioi_tinh nvarchar(10),
+@ma_tinh char(10),
+@ma_huyen char(10),
+@ma_xa char(10)
+as
+begin transaction
+	if(@sdt = NULL or @ten = NULL or @cmnd = NULL or @bien_so_xe = NULL)
+	begin
+		print N'Không được để trống các thông tin quan trọng'
+		rollback transaction
+		return
+	end
+	if not exists (select * from DIACHI where DC_MATINH = @ma_tinh)
+	begin 
+		print N'mã tỉnh ' + @ma_tinh + N' không tồn tại'
+		rollback tran
+		return
+	end
+	if not exists (select * from DIACHI where DC_MAHUYEN = @ma_huyen)
+	begin 
+		print N'mã huyện ' + @ma_huyen + N' không tồn tại'
+		rollback tran
+		return
+	end
+	if not exists (select * from DIACHI where DC_MAXA = @ma_xa)
+	begin 
+		print N'mã xã ' + @ma_xa + N' không tồn tại'
+		rollback tran
+		return
+	end
+	declare @stt int
+	declare @ma char(10)
+	select @stt = count(TX_MA) + 1 from TAIXE
+	set @ma = 'tx_' + cast(@stt as char(10))
+	--Insert
+	insert into TAIXE values(@ma,@ten, @cmnd, @sdt, @bien_so_xe, @stk, @ngan_hang,@gioi_tinh,@ma_tinh,@ma_huyen,@ma_xa)
+	print 'Thêm thành công'
+commit transaction
+go
+--Xem thông tin của tài xế
+create proc sp_xemthongtintaixe
+@ma char(10)
+as
+begin transaction
+	if(@ma = NULL )
+	begin
+		print N'Không nhận được mã'
+		rollback transaction
+		return
+	end
+	if not exists(select * from TAIXE where TX_MA=@ma)
+	begin
+		print N'Mã tài xế ' + @makh + N' không tồn tại'
+		rollback transaction
+		return
+	end
+	--Select
+	select * from TAIXE where TX_MA=@ma
+commit transaction
+go
+--Cập nhật thông tin tài xế
+create proc sp_capnhatthongtintaixe
+@ma char(10),
+@ten nvarchar(10),
+@cmnd char(20),
+@sdt char(10),
+@bien_so_xe char(10),
+@stk char(20),
+@ngan_hang nvarchar(100),
+@gioi_tinh nvarchar(10),
+@ma_tinh char(10),
+@ma_huyen char(10),
+@ma_xa char(10)
+as
+begin transaction
+	if(@ma = NULL or @sdt = NULL or @ten = NULL or @cmnd = NULL or @bien_so_xe = NULL)
+	begin
+		print N'Không được để trống các thông tin quan trọng'
+		rollback transaction
+		return
+	end
+	if not exists(select * from TAIXE where TX_MA=@ma)
+	begin
+		print N'Mã tài xế ' +@makh + N' không tồn tại'
+		rollback transaction
+		return
+	end
+	if not exists (select * from DIACHI where DC_MATINH = @ma_tinh)
+	begin 
+		print N'mã tỉnh ' + @ma_tinh + N' không tồn tại'
+		rollback tran
+		return
+	end
+	if not exists (select * from DIACHI where DC_MAHUYEN = @ma_huyen)
+	begin 
+		print N'mã huyện ' + @ma_huyen + N' không tồn tại'
+		rollback tran
+		return
+	end
+	if not exists (select * from DIACHI where DC_MAXA = @ma_xa)
+	begin 
+		print N'mã xã ' + @ma_xa + N' không tồn tại'
+		rollback tran
+		return
+	end
+	--Update
+	update TAIXE set
+	TX_MA=@ma,
+	TX_TEN=@ten, 
+	TX_CMND=@cmnd, 
+	TX_SDT=@sdt, 
+	TX_BIENSOXE=@bien_so_xe, 
+	TX_STK=@stk, 
+	TX_NGANHANG=@ngan_hang,
+	TX_GIOITINH=@gioi_tinh,
+	DC_MATINH=@ma_tinh,
+	DC_MAHUYEN=@ma_huyen,
+	DC_MAXA=@ma_xa
+	print 'Cập nhật thành công'
+commit transaction
+go
+--Hiển thị ra danh sách đơn hàng chưa được nhận giao
+create proc sp_xemdanhsachdonhang
+as
+begin transaction
+	--Select
+	select * from TINHTRANGGIAOHANG where TTGH_TINHTRANG=N'chưa xác nhận đơn hàng' and TX_MA=NULL
+commit transaction
+go
+--Xử lý đơn đặt hàng 
+create proc sp_xulydonhang
+@ma_tx char(10),
+@ma_dh char(10),
+@tinhtrang nvarchar(50)
+as
+begin transaction
+	if(@ma_dh = NULL)
+	begin
+		print N'Không nhận được mã đơn hàng'
+		rollback transaction
+		return
+	end
+	if(@ma_tx = NULL)
+	begin
+		print N'Không nhận được mã tài xế'
+		rollback transaction
+		return
+	end
+	if(@tinhtrang = NULL )
+	begin
+		print N'Không được để trống'
+		rollback transaction
+		return
+	end
+	if not exists(select * from TINHTRANGGIAOHANG where DH_MA=@ma_dh)
+	begin
+		print N'Không có đơn hàng mã ' + @ma_dh 
+		rollback transaction
+		return
+	end
+	if exists(select * from TINHTRANGGIAOHANG where DH_MA=@ma_dh and TTGH_TINHTRANG=N'Đã nhận đơn hàng')
+	begin
+		print N'Mã đơn hàng ' + @ma_dh + N' đã nhận'
+		rollback transaction
+		return
+	end
+	if exists(select * from TINHTRANGGIAOHANG where DH_MA=@ma_dh and TX_MA<>@ma_tx  and TTGH_TINHTRANG=N'Đã nhận đơn hàng')
+	begin
+		print N'Mã đơn hàng ' + @ma_dh + N' đã có tài xế nhận'
+		rollback transaction
+		return
+	end
+	--Update tình trạng giao hàng
+	if(@tinhtrang = N'Đã nhận đơn hàng')
+	begin
+		update TINHTRANGGIAOHANG set TX_MA=@ma_tx, TTGH_TINHTRANG=@tinhtrang
+	end
+	else
+	begin
+		update TINHTRANGGIAOHANG set TX_MA=NULL , TTGH_TINHTRANG=N'chưa nhận đơn hàng'
+	end
+	print 'Cập nhật tình trạng  thành công'
+commit transaction
+go
+--Những đơn hàng đã nhận để kiểm tra và giao hàng
+create proc sp_donhangtaixenhan
+@ma_tx char(10)
+as
+begin transaction
+	if(@ma_tx = NULL )
+	begin
+		print N'Không nhận được mã'
+		rollback transaction
+		return
+	end
+	if not exists(select * from TAIXE where TX_MA=@ma_tx)
+	begin
+		print N'Mã tài xế ' + @ma + N' không tồn tại'
+		rollback transaction
+		return
+	end
+	--Select
+	select * from TINHTRANGGIAOHANG where TX_MA=@ma_tx and TTGH_TINHTRANG=N'Đã nhận đơn hàng'
+commit transaction
+go
+--Tiền đơn hàng tài xế nhận
+create proc sp_tiendonhangtaixenhan
+@ma_tx char(10),
+@ma_dh char(10),
+@money int out
+as
+begin transaction
+	if(@ma_tx = NULL)
+	begin
+		print N'Không nhận được mã'
+		rollback transaction
+		return
+	end
+	if not exists(select * from TAIXE where TX_MA=@ma_tx)
+	begin
+		print N'Mã tài xế ' + @ma + N' không tồn tại'
+		rollback transaction
+		return
+	end
+	--Select
+	select @money=DH_TONGTIEN from DONHANG where DH_MA=@ma_dh
+commit transaction
+go
+--Tổng tiền đơn đã giao
+create proc sp_tongtien
+@ma char(10),
+@sum_money int out
+as
+begin transaction
+	if(@ma = NULL )
+	begin
+		print N'Không nhận được mã'
+		rollback transaction
+		return
+	end
+	if not exists(select * from TAIXE where TX_MA=@ma)
+	begin
+		print N'Mã tài xế ' + @ma + N' không tồn tại'
+		rollback transaction
+		return
+	end
+	--Select
+	select @sum_money=sum(DH_TONGTIEN) from TINHTRANGGIAOHANG left join DONHANG on TINHTRANGGIAOHANG.DH_MA=DONHANG.DH_MA where TINHTRANGGIAOHANG.TX_MA=@ma and TINHTRANGGIAOHANG.TTGH_TINHTRANG=N'Đã nhận đơn hàng'
+commit transaction
+go
+
