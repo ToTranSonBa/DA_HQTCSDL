e2da7168e0486bbf5e740d2b7c829705cae4583c
QuocMaChe <quocmache@gmail.com>
2022-12-04 20:48:43 +0700

    table.sql

--- DATABASE/table.sql
+++ DATABASE/table.sql
@@ -45,7 +45,7 @@ GO
 CREATE 
 --ALTER
 TABLE DOITAC ( 
-	MA CHAR(10) PRIMARY KEY,
+	DT_MA CHAR(10) PRIMARY KEY,
 	DT_TEN NVARCHAR(50),
 	DT_SDT CHAR(10),
 	DT_EMAIL NVARCHAR(100)
@@ -55,7 +55,7 @@ GO
 CREATE 
 --ALTER
 TABLE NHANVIEN (
-	MA CHAR(10) PRIMARY KEY,
+	NV_MA CHAR(10) PRIMARY KEY,
 	NV_TEN NVARCHAR(50),
 	NV_NGAYSINH DATE,
 	NV_CMND CHAR(20),
@@ -86,7 +86,7 @@ CREATE
 TABLE CUAHANG (
 	CH_MA CHAR(10) PRIMARY KEY,
 	DT_MA CHAR(10),
-	CH_TEN CHAR(50), 
+	CH_TEN CHAR(10), 
 	CH_TGMOCUA TIME,
 	CH_TGDONGCUA TIME,
 	CH_SDT CHAR(10),
@@ -113,7 +113,7 @@ TABLE THUCDON (
 	TD_MA CHAR(10),		
 	CN_MA CHAR(10), 
 	CH_MA CHAR(10),
-	TD_TEN CHAR(50)
+	TD_TEN CHAR(10)
 	CONSTRAINT PK_THUCDON PRIMARY KEY (TD_MA,CN_MA,CH_MA)
 )
 GO
@@ -128,7 +128,6 @@ TABLE MONAN (
 	MAN_TEN NVARCHAR(100),
 	MAN_MIEUTA NVARCHAR(100),
 	MAN_TINHTRANG NVARCHAR(20),
-	MAN_ANH NVARCHAR(50),
 	MAN_GIA MONEY,
 	CONSTRAINT PK_MONAN PRIMARY KEY (MAN_MA, TD_MA,CN_MA,CH_MA)
 )
@@ -170,8 +169,8 @@ GO
 CREATE
 --ALTER
 TABLE TAIXE (
-	MA CHAR(10) PRIMARY KEY,
-	TX_TEN NVARCHAR(50),
+	TX_MA CHAR(10) PRIMARY KEY,
+	TX_TEN NVARCHAR(10),
 	TX_CMND CHAR(20),
 	TX_SDT CHAR(10),
 	TX_BIENSOXE CHAR(10),
@@ -203,10 +202,10 @@ GO
 CREATE 
 --ALTER 
 TABLE KHACHHANG (
-	MA CHAR(10) PRIMARY KEY,
-	KH_TEN NVARCHAR(50),
+	KH_MA CHAR(10) PRIMARY KEY,
+	KH_TEN NVARCHAR(10),
 	KH_SDT CHAR(10),
-	KH_MAIL CHAR(50),
+	KH_MAIL CHAR(10),
 	KH_GIOITINH NVARCHAR(10),
 	--DIACHI
 	DC_MATINH CHAR(10),
@@ -215,6 +214,8 @@ TABLE KHACHHANG (
 	
 )
 GO
+
+
 
 CREATE
 --ALTER
@@ -230,72 +231,5 @@ TABLE DIACHI (
 )
 GO
 
-CREATE
---ALTER
-TABLE TAIKHOAN(
-	MA CHAR(10) NOT NULL PRIMARY KEY,
-	UNAME CHAR(20) NOT NULL,
-	PWD CHAR(10) NOT NULL,
-	LOAI CHAR(10) NOT NULL
-)
-GO
-
-insert into TAIKHOAN values('kh_01','zp19d0z','1234','KHACHHANG');
-insert into TAIKHOAN values('kh_02','20120429','1234','KHACHHANG');
-insert into TAIKHOAN values('kh_03','20120000','1234','KHACHHANG');
-insert into TAIKHOAN values('kh_04','20120001','1234','KHACHHANG');
-insert into TAIKHOAN values('kh_05','20120002','1234','KHACHHANG');
-
-insert into TAIKHOAN values('dt_01','20120008','1234','DOITAC');
-insert into TAIKHOAN values('dt_02','20120007','1234','DOITAC');
-insert into TAIKHOAN values('nv_01','20120003','1234','NHANVIEN');
-insert into TAIKHOAN values('nv_02','20120004','1234','NHANVIEN');
-insert into TAIKHOAN values('tx_01','20120005','1234','TAIXE');
-insert into TAIKHOAN values('tx_02','20120006','1234','TAIXE');
 
 
-
-insert into DIACHI values('11','1','1',N'Cao Bằng','abc','bcd',NULL);
-insert into DIACHI values('70','1','1',N'Tây Ninh','efg','abs',NULL);
-insert into DIACHI values('59','1','1',N'TP. Hồ Chí Minh','ekj','lop',NULL);
-
-insert into KHACHHANG values('kh_01',N'Nguyễn A','0395639633','abc@gmail',N'Nam','11','1','1');
-insert into KHACHHANG values('kh_02',N'Nguyễn B','0395639611','abc1@gmail',N'Nam','11','1','1');
-insert into KHACHHANG values('kh_03',N'Nguyễn C','0395639612','abc2@gmail',N'Nam','59','1','1');
-insert into KHACHHANG values('kh_04',N'Nguyễn D','0395639613','abc3@gmail',N'Nam','11','1','1');
-insert into KHACHHANG values('kh_05',N'Nguyễn E','0395639614','abc4@gmail',N'Nam','70','1','1');
-
-insert into DOITAC values('dt_01',N'Nguyễn Văn F','0395639615','ab5c@gmail.com');
-insert into DOITAC values('dt_02',N'Nguyễn Văn G','0395639616','abc6@gmail.com');
-insert into NHANVIEN values('nv_01',N'Nguyễn Văn H','2002-01-11','5809237594','0395639617',N'Nam','abc7@gmail.com','59','1','1');
-insert into NHANVIEN values('nv_02',N'Nguyễn Văn J','2002-02-22','5809345367','0395639618',N'Nam','abc8@gmail.com','11','1','1');
-insert into TAIXE values('tx_01',N'Nguyễn K','134804358994','0395639619','j29-4565','1458912432',N'MB Bank',N'Nam','70','1','1');
-insert into TAIXE values('tx_02',N'Nguyễn L','134809135995','0395639620','j29-3479','1458947598',N'Agribank',N'Nam','11','1','1');
-
-insert into THUCDON values('td_01','cn_03','ch_01',N'Menu Hambu');
-insert into THUCDON values('td_02','cn_02','ch_05',N'Menu Pizza');
-
-insert into CHINHANH values('cn_03','ch_01','59','1','1',N'Chi Nhánh Tây Ninh');
-insert into CHINHANH values('cn_02','ch_04','70','1','1',N'Chi Nhánh TP Hồ Chí Minh');
-
-insert into CUAHANG values('ch_01','dt_01','Hambu đê','05:59:59','23:59:59','1999999990',NULL);
-insert into CUAHANG values('ch_04','dt_01','Hambu đê','05:59:59','23:59:59','1999999990',NULL);
-insert into CUAHANG values('ch_05','dt_02','Pizza đê','05:59:59','23:59:59','1989898989',NULL);
-
-insert into MONAN values('mn_01','td_01','cn_03','ch_01',N'Hamburger Cá','abc',NULL,NULL,89999);
-insert into MONAN values('mn_02','td_01','cn_03','ch_04',N'Hamburger Bò','efg',NULL,NULL,99999);
-insert into MONAN values('mn_03','td_02','cn_02','ch_05',N'Pizza Thập Cẩm','ekj',NULL,NULL,199999);
-
-
-select count(*) from TAIKHOAN where UNAME='zp19d0z' and PWD='1234'
-go
-create proc sp_taikhoan 
-@username char(20),
-@password char(10)
-As
-select * from TAIKHOAN where UNAME='zp19d0z' and PWD='1234'
-
-exec sp_taikhoan 'zp19d0z','1234';
-
-
-select * from TAIKHOAN where UNAME='zp19d0z' and PWD=1234
